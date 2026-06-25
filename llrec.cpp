329609ffcd0b6ae725d309b2a94e5ac98e9aa89d
ebalyan <ebalyan@usc.edu>
2026-06-25 16:36:04 -0700

    llrec.cpp

--- llrec.cpp
+++ llrec.cpp
@@ -4,13 +4,14 @@
 // Provide your implementation of llpivot below
 //*********************************************
 
-void llpivot(Node*& head, Node*& smaller, Node*& larger, int pivot)
+void llpivot_helper(Node*& head, Node*& smaller, Node*& larger, int pivot)
 {
   if (head == NULL) {
     smaller = NULL;
     larger = NULL;
     return;
   }
+
   Node* curr = head;
   head = head->next;
   curr->next = NULL;
@@ -19,16 +20,16 @@ void llpivot(Node*& head, Node*& smaller, Node*& larger, int pivot)
     smaller = curr;
     llpivot_helper(head, smaller->next, larger, pivot);
   }
-  else {        
+  else {
     larger = curr;
     llpivot_helper(head, smaller, larger->next, pivot);
-    }
+  }
 }
 
 void llpivot(Node*& head, Node*& smaller, Node*& larger, int pivot)
 {
-    smaller = NULL;
-    larger = NULL;
+  smaller = NULL;
+  larger = NULL;
 
-    llpivot_helper(head, smaller, larger, pivot);
+  llpivot_helper(head, smaller, larger, pivot);
 }
